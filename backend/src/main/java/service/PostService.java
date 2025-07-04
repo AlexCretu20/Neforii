d84b898b7ece362e1e059788ff9abaa72351549d
AlexCretu20 <alexmari.cretu@gmail.com>
2025-07-04 16:36:02 +0300

    Created Service Post

--- backend/src/main/java/service/PostService.java
+++ backend/src/main/java/service/PostService.java
@@ -1,4 +1,104 @@
 package service;
 
+import model.Comment;
+import model.Post;
+import model.User;
+import model.Vote;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class PostService {
+
+   private static PostService instance;
+   private static  int counter;
+   private Map<Integer, Post> posts = new HashMap<>();
+   private PostService() {
+       counter = 0;
+   };
+
+    public static PostService getInstance() {
+        if (instance == null){
+            instance = new PostService();
+        }
+        return instance;
+    }
+
+    public Integer GetPostById(Post post) {
+        for (Map.Entry<Integer, Post> entry : posts.entrySet()) {
+            if (entry.getValue().equals(post)) {
+                return entry.getKey();
+            }
+        }
+        System.out.println("The post doesn't exist. ");
+        return null;
+    }
+
+    public void CreatePost(User user, String text){
+        counter ++;
+        Post post = new Post(counter, text, LocalDateTime.now(),false, user);
+        posts.put(counter, post);
+        System.out.println("The post was created.");
+    }
+
+    public void UpdatePost(int id, String text){
+        for (Integer key: posts.keySet()){
+            if (key == id){
+                posts.get(key).setText(text);
+            }
+
+        }
+        System.out.println("The post was updated");
+
+    }
+
+    public void DeletePost(int id){
+       posts.remove(id);
+       System.out.println("The post was deleted.");
+    }
+
+    public void AddComment(int id, Comment comment){
+        for (Integer key: posts.keySet()){
+            if (key == id){
+                posts.get(key).setComments(comment);
+            }
+
+        }
+        System.out.println("The comment was added.");
+    }
+
+    public void AddVote(int id, Vote vote){
+        for (Integer key: posts.keySet()){
+            if (key == id){
+                posts.get(key).setVotes(vote);
+            }
+
+        }
+        System.out.println("The vote was added.");
+    }
+
+    public void ExpandComments(int id){
+        ArrayList<Comment> comments = new ArrayList<>();
+        for (Integer key: posts.keySet()){
+            if (key == id){
+                comments = posts.get(key).getComments();
+            }
+
+        }
+
+        System.out.println("Comments");
+        for (Comment cm: comments){
+            System.out.println(cm.getText());
+        }
+
+    }
+
+    public void DisplayPosts(){
+        for (Map.Entry<Integer, Post> entry : posts.entrySet()) {
+            System.out.println(entry.getValue());
+        }
+
+    }
 }
--- personal/src/main/java/Alexandra/Test.java
+++ personal/src/main/java/Alexandra/Test.java
@@ -1,14 +1,38 @@
 package Alexandra;
 
-import model.Post;
+import model.Comment;
+import model.EntityType;
+import model.User;
+import model.Vote;
+import service.PostService;
 
 import java.time.LocalDateTime;
 
 public class Test {
     public static void main(String[] args) {
 
-        Post ps = new Post(2,"sdfghjkhgf", LocalDateTime.now(),false,null);
-        System.out.println(ps);
+
+//        Post ps = new Post(2,"sdfghjkhgf", LocalDateTime.now(),false,null);
+//        System.out.println(ps);
+
+        User user = new User("ana1234","xcvbj","ana@gmail.com","1234567","sdfghjkjhgfdcfvgbhjn tybunnhygtfyhujg");
+        Comment comment = new Comment(1,"salut ana", LocalDateTime.now(),LocalDateTime.now(),user);
+        Vote vote = new Vote(1,true, LocalDateTime.now(), EntityType.POST,1,1);
+
+        //test service
+        PostService postService = PostService.getInstance();
+
+        postService.CreatePost(user,"Ana are mere");
+        postService.CreatePost(user,"sdfghjhgfddfghjm");
+        postService.CreatePost(user,"capsuni si mere ");
+        postService.DisplayPosts();
+        postService.UpdatePost(2, "bauu saluuut");
+        postService.DeletePost(3);
+        postService.AddComment(1,comment);
+        postService.AddVote(1,vote);
+        postService.ExpandComments(1);
+        postService.DisplayPosts();
+
 
     }
 
