b4ef5aad5a362dd2463dc2e6c910806dc42b7982
alexelabassi <alex.tamer@yahoo.com>
2025-07-04 15:37:24 +0300

    Create/Delete votes

--- backend/src/main/java/Main.java
+++ backend/src/main/java/Main.java
@@ -5,28 +5,35 @@ public class Main {
 //        PostService postService = new PostService();
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Welcome to Neforii!");
-        System.out.println("1. Register");
-        System.out.println("2. Login");
-        System.out.println("0. Exit");
+        System.out.println("Welcome to Neforii!\n");
+        System.out.println("1. Register\n");
+        System.out.println("2. Login\n");
+        System.out.println("0. Exit\n");
         String choice = scanner.nextLine();
         switch (choice) {
             case "1": {
-                System.out.println("Register functionality is not implemented yet.");
+                System.out.println("Register functionality is not implemented yet.\n");
             }
             case "2": {
-                System.out.println("Login functionality is not implemented yet.");
+                System.out.println("Login functionality is not implemented yet.\n");
                 break;
             }
             case "0": {
-                System.out.println("Stopping the application!");
+                System.out.println("Stopping the application!\n");
                 return;
             }
         }
 
         while (true) {
-            System.out.println("1. Create Post");
-            break;
+            System.out.println("1. Creeaza o postare\n");
+            System.out.printf("2. Vezi toate postarile\n");
+            System.out.println("3. Sterge o postare\n");
+            System.out.println("4. Vizualizeaza o postare\n");
+            System.out.println("5. Comenteaza la o postare\n");
+            // comenteaza la un comentariu
+            // voteaza o postare
+            // voteaza un comentariu
+            // la 10 voturi pozitive ceva frumos
         }
 
     }
--- backend/src/main/java/service/VoteService.java
+++ backend/src/main/java/service/VoteService.java
@@ -1,4 +1,55 @@
 package service;
 
+import model.EntityType;
+import model.Vote;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class VoteService {
+    private static VoteService instance;
+    private final List<Vote> votes = new ArrayList<>();
+    private int counter = 0;
+
+
+    private VoteService() {
+    }
+
+    public static VoteService getInstance() {
+        if (instance == null) {
+            instance = new VoteService();
+        }
+        return instance;
+    }
+
+    public String createVote(int userId, EntityType entityType, int entityId, boolean isUpvote) {
+        votes.removeIf(vote -> vote.getUserId() == userId && vote.getEntityId() == entityId && vote.getEntityType() == entityType);
+
+        Vote vote = new Vote(
+                counter++,
+                isUpvote,
+                LocalDateTime.now(),
+                entityType,
+                entityId,
+                userId
+        );
+        votes.add(vote);
+        return "You have successfully voted!";
+    }
+
+    public String deleteVote(int userId, EntityType entityType, int entityId) {
+        boolean removed = votes.removeIf(vote -> vote.getUserId() == userId && vote.getEntityType() == entityType && vote.getEntityId() == entityId);
+        if (removed) {
+            return "Vote deleted successfully!";
+        } else {
+            return "No vote found to delete.";
+        }
+    }
+
+    public boolean hasUserVoted(int userId, EntityType entityType, int entityId) {
+        return votes.stream()
+                .anyMatch(vote -> vote.getUserId() == userId && vote.getEntityType() == entityType && vote.getEntityId() == entityId);
+    }
+
 }
