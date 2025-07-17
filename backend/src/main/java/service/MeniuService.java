b877bee76258ca40fa527f79f2b6022e8f04b0b1
bogdan_balasoiu <balasoiu.bogdan@gmail.com>
2025-07-17 13:38:19 +0300

    Validation Methods in Menu so you can not introduce a wrong id

--- backend/src/main/java/service/MeniuService.java
+++ backend/src/main/java/service/MeniuService.java
@@ -1,25 +1,28 @@
 package service;
 
+import exception.CommentNotFoundException;
+import model.Post;
 import ui.UserUI;
 
 import java.util.Scanner;
 
 public class MeniuService {
-    private CommentService commentService;
-    private PostService postService;
-    private UserService userService;
-    private VoteService voteService;
-    private Scanner scanner;
-    private UserUI userUI;
+    private final CommentService commentService;
+    private final PostService postService;
+    private final UserService userService;
+    private final VoteService voteService;
+    private final Scanner scanner;
+    private final UserUI userUI;
 
-    public MeniuService(CommentService commentService, PostService postService, UserService userService, VoteService voteService, Scanner scanner, UserUI userUI) {
+    public MeniuService(CommentService commentService, PostService postService,
+                        UserService userService, VoteService voteService,
+                        Scanner scanner, UserUI userUI) {
         this.commentService = commentService;
         this.postService = postService;
         this.userService = userService;
         this.voteService = voteService;
         this.scanner = scanner;
         this.userUI = userUI;
-
     }
 
     private void tryToLogin() {
@@ -30,7 +33,6 @@ public class MeniuService {
     }
 
     public void displayLoginMeniu() {
-
         boolean flag = true;
         while (flag) {
             System.out.println("Welcome to Neforii!\n");
@@ -38,43 +40,36 @@ public class MeniuService {
                 System.out.println("1. Register");
                 System.out.println("2. Login");
                 System.out.println("0. Exit");
-                System.out.println("Enter your choice: ");
+                System.out.print("Enter your choice: ");
                 String choice = scanner.nextLine();
                 switch (choice) {
-                    case "1":
+                    case "1" -> {
                         userUI.registerUserUI();
                         tryToLogin();
-                        break;
-
-                    case "2":
-                        tryToLogin();
-                        break;
-
-                    case "0":
+                    }
+                    case "2" -> tryToLogin();
+                    case "0" -> {
                         System.out.println("Stopping the application!\n");
                         flag = false;
-                        break;
-                    default:
-                        System.out.println("Invalid choice! Please try again!\n");
-
+                    }
+                    default -> System.out.println("Invalid choice! Please try again!\n");
                 }
             } else {
                 System.out.println("1. Logout");
                 System.out.println("0. Exit");
-                System.out.println("Enter your choice: ");
+                System.out.print("Enter your choice: ");
                 String choice = scanner.nextLine();
                 switch (choice) {
-                    case "1":
+                    case "1" -> {
                         String username = userService.getCurrentUser().getUsername();
                         userService.logoutUser();
                         System.out.println(username + " has logged out! See you next time!");
-                        break;
-                    case "0":
+                    }
+                    case "0" -> {
                         System.out.println("Stopping the application!\n");
                         flag = false;
-                        break;
-                    default:
-                        System.out.println("Invalid choice! Please try again!\n");
+                    }
+                    default -> System.out.println("Invalid choice! Please try again!\n");
                 }
             }
         }
@@ -84,73 +79,53 @@ public class MeniuService {
         boolean flag = true;
 
         while (flag) {
+            System.out.println("\n--- Main Menu ---");
             System.out.println("1. Create a post");
             System.out.println("2. See all posts");
-            //daca un user are postari atunci ii apare in meniu optiunea de editarea/stergerea a postarii
             System.out.println("3. Update a post");
             System.out.println("4. Delete a post");
             System.out.println("5. See a post");
             System.out.println("0. Back");
-            System.out.println("Enter your choice: ");
+            System.out.print("Enter your choice: ");
             String choice = scanner.nextLine();
 
-
             switch (choice) {
-
-                case "1":
-                    System.out.println("Enter your text for the post:");
+                case "1" -> {
+                    System.out.print("Enter your text for the post: ");
                     String text = scanner.nextLine();
                     postService.createPost(userService.getCurrentUser(), text);
-                    break;
-
-                case "2":
-                    postService.displayPosts();
-                    break;
-
-                case "3":
-                    // un user poate modifica doar o postare a lui
-                    System.out.println("Enter the id of the post you want to modify");
-                    int number = scanner.nextInt();
-                    scanner.nextLine();
-
-                    if (!userService.getCurrentUser().equals(postService.getPostById(number).getUser())) {
-                        System.out.println("You can't modify a post made by another user ");
+                }
+                case "2" -> postService.displayPosts();
+                case "3" -> {
+                    int postId = readValidPostId("Enter the ID of the post you want to modify: ");
+                    Post post = postService.getPostById(postId);
+                    if (!userService.getCurrentUser().equals(post.getUser())) {
+                        System.out.println("You can't modify a post made by another user.");
                     } else {
-                        System.out.println("Enter the new text: ");
+                        System.out.print("Enter the new text: ");
                         String newText = scanner.nextLine();
-                        postService.updatePost(number, newText);
+                        postService.updatePost(postId, newText);
                     }
-                    break;
-
-                case "4":
-                    // un user poate sterge doar o postare a lui
-                    System.out.println("Enter the id of the post you want to modify");
-                    int number2 = scanner.nextInt();
-                    scanner.nextLine();
-                    if (!userService.getCurrentUser().equals(postService.getPostById(number2).getUser())) {
-                        System.out.println("You can't delete a post made by another user ");
+                }
+                case "4" -> {
+                    int postId = readValidPostId("Enter the ID of the post you want to delete: ");
+                    Post post = postService.getPostById(postId);
+                    if (!userService.getCurrentUser().equals(post.getUser())) {
+                        System.out.println("You can't delete a post made by another user.");
                     } else {
-                        postService.deletePost(number2);
+                        postService.deletePost(postId);
                     }
-                    break;
-
-                case "5":
-                    System.out.println("Enter the number of a post:\n");
-                    int number3 = scanner.nextInt();
-                    scanner.nextLine();
-                    //sa se afiseze numarul de upvotes, downvotes, comenatrii
-                    postService.displayOnePost(number3);
-                    displayPostMenu(number3);
-                    break;
-
-                case "0":
-                    flag = false;
-                    break;
+                }
+                case "5" -> {
+                    int postId = readValidPostId("Enter the ID of the post to view: ");
+                    postService.displayOnePost(postId);
+                    displayPostMenu(postId);
+                }
+                case "0" -> flag = false;
+                default -> System.out.println("Invalid option.");
             }
-
         }
     }
-
 
     public void displayPostMenu(int postId) {
         boolean flag = true;
@@ -166,17 +141,13 @@ public class MeniuService {
             String choice = scanner.nextLine();
 
             switch (choice) {
-                case "1":
-                    commentService.showCommentsForPost(postId);// afișare recursiva comentarii
-                    break;
-
-                case "2":
+                case "1" -> commentService.showCommentsForPost(postId);
+                case "2" -> {
                     System.out.print("Write your comment: ");
                     String text = scanner.nextLine();
                     commentService.createCommentOnPost(text, userService.getCurrentUser(), postId);
-                    break;
-
-                case "3":
+                }
+                case "3" -> {
                     System.out.println("1. Upvote");
                     System.out.println("2. Downvote");
                     String voteChoice = scanner.nextLine();
@@ -185,25 +156,16 @@ public class MeniuService {
                         String result = voteService.createVote(userService.getCurrentUser().getId(), postId, null, isUpvote);
                         System.out.println(result);
                     }
-                    break;
-
-                case "4":
-                    System.out.print("Enter the ID of the comment to interact with: ");
-                    int commentId = Integer.parseInt(scanner.nextLine());
-                    commentService.getComment(commentId);
-                    displayCommentMenu(commentId); // meniul pentru comentariu selectat
-                    break;
-
-                case "0":
-                    flag = false;
-                    break;
-
-                default:
-                    System.out.println("Invalid option.");
+                }
+                case "4" -> {
+                    int commentId = readValidCommentId("Enter the ID of the comment to interact with: ");
+                    displayCommentMenu(commentId);
+                }
+                case "0" -> flag = false;
+                default -> System.out.println("Invalid option.");
             }
         }
     }
-
 
     public void displayCommentMenu(int commentId) {
         boolean flag = true;
@@ -218,13 +180,12 @@ public class MeniuService {
             String choice = scanner.nextLine();
 
             switch (choice) {
-                case "1":
+                case "1" -> {
                     System.out.print("Write your reply: ");
                     String replyText = scanner.nextLine();
                     commentService.createReplyToComment(replyText, userService.getCurrentUser(), commentId);
-                    break;
-
-                case "2":
+                }
+                case "2" -> {
                     System.out.println("1. Upvote");
                     System.out.println("2. Downvote");
                     String voteChoice = scanner.nextLine();
@@ -233,22 +194,44 @@ public class MeniuService {
                         String result = voteService.createVote(userService.getCurrentUser().getId(), null, commentId, isUpvote);
                         System.out.println(result);
                     }
-                    break;
-
-                case "3":
-                    commentService.showCommentsForComment(commentId);
-                    break;
-
-                case "0":
-                    flag = false;
-                    break;
-
-                default:
-                    System.out.println("Invalid option.");
+                }
+                case "3" -> commentService.showCommentsForComment(commentId);
+                case "0" -> flag = false;
+                default -> System.out.println("Invalid option.");
             }
         }
     }
 
-//    public void displayReplyMenu
+    private int readValidPostId(String prompt) {
+        int postId;
+        while (true) {
+            try {
+                System.out.print(prompt);
+                postId = Integer.parseInt(scanner.nextLine());
+                if (postService.getPostById(postId) == null) {
+                    System.out.println("No post found with that ID. Try again.");
+                } else {
+                    return postId;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid number. Please enter a valid post ID.");
+            }
+        }
+    }
 
+    private int readValidCommentId(String prompt) {
+        int commentId;
+        while (true) {
+            try {
+                System.out.print(prompt);
+                commentId = Integer.parseInt(scanner.nextLine());
+                commentService.getComment(commentId);
+                return commentId;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid number. Please enter a valid comment ID.");
+            } catch (CommentNotFoundException e) {
+                System.out.println("No comment found with that ID. Try again.");
+            }
+        }
+    }
 }
