af4d31f6ae356d819b50a7e28e8e29cf353df1b2
alexelabassi <alex.tamer@yahoo.com>
2025-07-29 08:39:41 +0300

    restructured image processor and added grayscale filter

---
+++ image-processing-pipeline/Filters/DoNothingFilter/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+using AppCore;
+
+namespace DoNothingFilter;
+
+public class DoNothingFilter : IFilter
+{ 
+    public Bitmap Apply(Bitmap inputImage)
+    {
+        return inputImage;
+    }
+}
---
+++ image-processing-pipeline/ImageProcessingEntrypoint/PathConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace ImageProcessingEntrypoint;
+
+public static class PathConfig
+{
+    public static readonly string BaseDir = AppContext.BaseDirectory; // entrypoint/bin/debug/net9.0
+    public static readonly string RootDir = Path.GetFullPath(Path.Combine(BaseDir, "..", "..", "..", ".."));
+    public static readonly string FiltersDir = Path.GetFullPath(Path.Combine(RootDir, "Plugins", "net9.0")); // plugins
+    public static readonly string InputDir = Path.Combine(RootDir, "Images", "Input");
+    public static readonly string OutputDir = Path.Combine(RootDir, "Images", "Output");
+    public static readonly string[] ImageExtensions = { ".jpg", ".png" };
+    public static readonly string[] FilterDlls = Directory.GetFiles(FiltersDir, "*.dll");
+    
+}
