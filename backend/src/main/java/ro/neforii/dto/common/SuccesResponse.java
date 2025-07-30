f5fc90186e77ef8dc3b543caaec5afc8f699cf79
alexelabassi <alex.tamer@yahoo.com>
2025-07-30 14:53:01 +0300

    gitignore changes

---
+++ backend/src/main/java/ro/neforii/dto/common/SuccesResponse.java
@@ -0,0 +1,13 @@
+﻿package ro.neforii.dto.common;
+
+import java.util.List;
+
+public class SuccesResponse<T> {
+    private boolean success;
+    private List<T> data;
+
+    public SuccesResponse(boolean success, List<T> data) {
+        this.success = success;
+        this.data = data;
+    }
+}
