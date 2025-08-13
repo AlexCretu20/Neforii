a771888929065499bba45040099af9bef5125ea6
AlexCretu20 <alexmari.cretu@gmail.com>
2025-08-13 15:31:05 +0300

    Add Home Controller and add nuget

---
+++ image-processing-pipeline/ImageProcessingEntrypoint/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ImageProcessingPipeline.Controllers
+{
+    [ApiController]
+    [Route("api/hello")]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetHello()
+        {
+            return Ok(new { message = "Hello World!" });
+        }
+    }
+}
--- image-processing-pipeline/ImageProcessingEntrypoint/Program.cs
+++ image-processing-pipeline/ImageProcessingEntrypoint/Program.cs
@@ -1,20 +1,33 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Drawing;
-using System.Security.AccessControl;
-using AppCore;
+﻿// using System;
+// using System.IO;
+// using System.Linq;
+// using System.Reflection;
+// using System.Drawing;
+// using System.Security.AccessControl;
+// using AppCore;
+//
+// namespace ImageProcessingEntrypoint
+// {
+//     class Program
+//     {
+//         private static void Main(string[] args)
+//         {
+//             var filters = FilterLoader.LoadAll(PathConfig.FiltersDir);
+//             ImageProcessingService.ProcessImages(filters, PathConfig.InputDir, PathConfig.OutputDir,
+//                 PathConfig.ImageExtensions); // aplica toate filtrele pe toate imaginile momentan
+//         }
+//     }
+// }
 
-namespace ImageProcessingEntrypoint
-{
-    class Program
-    {
-        private static void Main(string[] args)
-        {
-            var filters = FilterLoader.LoadAll(PathConfig.FiltersDir);
-            ImageProcessingService.ProcessImages(filters, PathConfig.InputDir, PathConfig.OutputDir,
-                PathConfig.ImageExtensions); // aplica toate filtrele pe toate imaginile momentan
-        }
-    }
-}
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
