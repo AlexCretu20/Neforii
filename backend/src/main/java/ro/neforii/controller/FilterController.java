4fd432a66981a32ae25d009b73ff42904e1bb3eb
AlexCretu20 <alexmari.cretu@gmail.com>
2025-08-20 13:18:37 +0300

    Refactor filter

--- backend/src/main/java/ro/neforii/controller/FilterController.java
+++ backend/src/main/java/ro/neforii/controller/FilterController.java
@@ -1,7 +1,5 @@
 package ro.neforii.controller;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,36 +8,22 @@ import ro.neforii.dto.common.ExpectedResponse;
 import ro.neforii.dto.filter.FilterDto;
 import ro.neforii.service.FilterService;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
 public class FilterController {
     private final FilterService filterService;
-    private final ObjectMapper objectMapper;
 
-    public FilterController(FilterService filterService, ObjectMapper objectMapper) {
+    public FilterController(FilterService filterService) {
         this.filterService = filterService;
-        this.objectMapper = objectMapper;
     }
 
     @GetMapping
     @RequestMapping("/filters")
-    public ResponseEntity<ExpectedResponse<List<FilterDto>>> getFilters() throws IOException, InterruptedException {
-        String jsonResponse = filterService.getFilters();
+    public ResponseEntity<ExpectedResponse<List<FilterDto>>> getFilters() {
 
-        JsonNode root = objectMapper.readTree(jsonResponse);
-
-        // verifica json nu e gol
-        JsonNode innerData = root.path("data");
-
-        List<FilterDto> filters = objectMapper.readValue(
-                innerData.toString(),
-                objectMapper.getTypeFactory().constructCollectionType(List.class, FilterDto.class)
-        );
-
+        List<FilterDto> filters = filterService.getFilters();
         return ResponseEntity.ok(new ExpectedResponse<>(filters));
 
     }
 }
-
